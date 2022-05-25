09f81c87c0266fd25159d81b3b1032f90fc0c5b3
minhduc0711 <phamminhduc0711@gmail.com>
2022-05-25 22:03:44 +0000

    Big code dump

--- src/data/data_module.py
+++ src/data/data_module.py
@@ -17,56 +17,96 @@ class SliderDataModule(pl.LightningDataModule):
         self.num_workers = num_workers
         self.batch_size = batch_size
 
-        n_mels = 128
-        frames = 5
-        n_fft = 1024
-        hop_length = 512
-        power = 2.0
+        self.n_mels = 128
+        self.n_fft = 1024
+        self.hop_length = 512
+        self.power = 2.0
         
-        subset_dirs = {
-            "train": data_dir / "dev_data/dev_data/slider/train",
-            "test": data_dir / "dev_data/dev_data/slider/test",
-            "predict": data_dir / "eval_data/eval_data/slider/test"
+        self.maxlen = maxlen
+        self.normalize = normalize
+        self.use_cnn = use_cnn
+        self.iter_over_cols = iter_over_cols
+        
+        self.subset_dirs = {
+            "dev_train": data_dir / "dev_data/dev_data/slider/train",
+            "dev_test": data_dir / "dev_data/dev_data/slider/test",
+            "eval_train": data_dir / "eval_data/eval_data/slider/train",
+            "eval_test": data_dir / "eval_data/eval_data/slider/test"
         }
         self.subsets = {}
-        for subset_name, subset_dir in subset_dirs.items():
-            ds = SliderDataset(subset_dir,
-                                n_mels=n_mels,
-                                frames=frames,
-                                n_fft=n_fft,
-                                hop_length=hop_length,
-                                power=power,
-                                maxlen=maxlen,
-                                use_cnn=use_cnn,
-                                normalize=normalize,
-                                iter_over_cols=iter_over_cols)
-            if subset_name == "train":
-                # also create a validation set
-                N = len(ds)
-                num_train = int(0.8 * N)
-                num_val = N - num_train
-                self.subsets["train"], self.subsets["val"] = \
-                        random_split(ds, [num_train, num_val])
-            else:
-                self.subsets[subset_name] = ds
-
+        self.machine_ids = {
+            "dev": ["00", "02", "04"],
+            "eval": ["01", "03", "05"],
+        }
+        # for subset_name, subset_dir in subset_dirs.items():
+        #     stage, split = subset_name.split("_")
+        #     for machine_id in self.machine_ids[stage]:
+        #         ds = SliderDataset(subset_dir,
+        #                             n_mels=n_mels,
+        #                             n_fft=n_fft,
+        #                             hop_length=hop_length,
+        #                             power=power,
+        #                             machine_id=machine_id,
+        #                             maxlen=maxlen,
+        #                             use_cnn=use_cnn,
+        #                             normalize=normalize,
+        #                             iter_over_cols=iter_over_cols)
+        #         if split == "train":
+        #             # also create a validation set
+        #             N = len(ds)
+        #             num_train = int(0.9 * N)
+        #             num_val = N - num_train
+        #             self.subsets[f"{stage}_train_{machine_id}"], self.subsets[f"{stage}_val_{machine_id}"] = \
+        #                     random_split(ds, [num_train, num_val])
+        #         else:
+        #             self.subsets[f"{subset_name}_{machine_id}"] = ds
+        self.active_subsets = {}
+    
+    def setup_subset(self, stage, machine_id):
+        if f"{stage}_train_{machine_id}" not in self.subsets.keys():
+            for subset_name in [f"{stage}_train", f"{stage}_test"]:
+                subset_dir = self.subset_dirs[subset_name]
+                ds = SliderDataset(subset_dir,
+                                    n_mels=self.n_mels,
+                                    n_fft=self.n_fft,
+                                    hop_length=self.hop_length,
+                                    power=self.power,
+                                    machine_id=machine_id,
+                                    maxlen=self.maxlen,
+                                    use_cnn=self.use_cnn,
+                                    normalize=self.normalize,
+                                    iter_over_cols=self.iter_over_cols)
+                if subset_name == f"{stage}_train":
+                    # also create a validation set
+                    N = len(ds)
+                    num_train = int(0.9 * N)
+                    num_val = N - num_train
+                    self.subsets[f"{stage}_train_{machine_id}"], self.subsets[f"{stage}_val_{machine_id}"] = \
+                            random_split(ds, [num_train, num_val])
+                else:
+                    self.subsets[f"{subset_name}_{machine_id}"] = ds
+            
+        self.active_subsets["train"] = self.subsets[f"{stage}_train_{machine_id}"]
+        self.active_subsets["val"] = self.subsets[f"{stage}_val_{machine_id}"]
+        self.active_subsets["test"] = self.subsets[f"{stage}_test_{machine_id}"]
+        
     def train_dataloader(self):
-        return DataLoader(self.subsets["train"], 
+        if "train" not in self.active_subsets:
+            raise RuntimeError("Please call setup_subset(stage, machine_id) first")
+        return DataLoader(self.active_subsets["train"], 
                           batch_size=self.batch_size,
                           num_workers=self.num_workers)
 
     def val_dataloader(self):
-        return DataLoader(self.subsets["val"], 
+        if "val" not in self.active_subsets:
+            raise RuntimeError("Please call setup_subset(stage, machine_id) first")
+        return DataLoader(self.active_subsets["val"], 
                           batch_size=self.batch_size,
                           num_workers=self.num_workers)
     
     def test_dataloader(self):
-        return DataLoader(self.subsets["test"], 
+        if "test" not in self.active_subsets:
+            raise RuntimeError("Please call setup_subset(stage, machine_id) first")
+        return DataLoader(self.active_subsets["test"], 
                           batch_size=self.batch_size,
                           num_workers=self.num_workers)
-    
-    def predict_dataloader(self):
-        return DataLoader(self.subsets["predict"], 
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers)
-
--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,7 @@
+import sys
 from pathlib import Path
 import re
-from tqdm import tqdm
+from tqdm.auto import tqdm
 import math
 from functools import lru_cache
 
@@ -20,47 +21,81 @@ class SliderDataset(Dataset):
         iter_over_cols: if True, each data item is a column from a spectrogram, 
                         otherwise each item is a melspectrogram corresponding to one audio file
     """
-    def __init__(self, dirs, n_mels, frames, n_fft, hop_length, power,
+    def __init__(self, dirs, n_mels, n_fft, hop_length, power,
                 normalize=False,
                 maxlen=313,
                 use_cnn=False,
-                iter_over_cols=False):
+                iter_over_cols=False,
+                machine_id: int = None):
         if type(dirs) != list:
             dirs = [dirs]
         self.dirs = dirs
         self.maxlen = maxlen
         self.normalize = normalize
         self.use_cnn = use_cnn
-        self.audio_paths = [audio_path for d in self.dirs for audio_path in sorted(Path(d).iterdir())]
+        
         self.prog = re.compile('(\w*)?_?id_(\d*)_(\d*)')
 
-        self.n_mels, self.frames, self.n_fft = n_mels, frames, n_fft
+        audio_paths = [audio_path for d in self.dirs for audio_path in sorted(Path(d).iterdir())]
+        # use only subset of audios belonging to 1 certain machine if wanted
+        if machine_id is None:
+            self.audio_paths = audio_paths
+        else:
+            self.audio_paths = []
+            for p in audio_paths:
+                regex_res = self.prog.match(p.name)
+                audio_machine_id = regex_res.group(2)
+                if audio_machine_id == machine_id:
+                    self.audio_paths.append(p)
+            
+        self.n_mels, self.n_fft = n_mels, n_fft
         self.hop_length, self.power = hop_length, power
         
         self.iter_over_cols = iter_over_cols
         if iter_over_cols:
-            idx2col = {}
-            num_cols = 0
+            # idx2col = {}
+            # num_cols = 0
+            # # n_sliding_frames = 5
+            # for file_idx, audio_path in enumerate(tqdm(self.audio_paths, 
+            #                                            desc="Building index for spectrogram columns")):
+            #     wav, sr = torchaudio.load(audio_path)
+            #     num_frames = math.ceil((sr / hop_length) * (wav.shape[1] / sr))
+            #     # num_frames = num_frames - n_sliding_frames + 1
+            #     for i in range(num_frames):
+            #         idx2col[i + num_cols] = (file_idx, i)
+            #     num_cols += num_frames
+            # self.idx2col = idx2col
+            # self.num_cols = num_cols
+            self.X = []
+            self.y = []
             for file_idx, audio_path in enumerate(tqdm(self.audio_paths, 
-                                                       desc="Building index for spectrogram columns")):
-                wav, sr = torchaudio.load(audio_path)
-                num_frames = math.ceil((sr / hop_length) * (wav.shape[1] / sr))
-                for i in range(num_frames):
-                    idx2col[i + num_cols] = (file_idx, i)
-                num_cols += num_frames
-            self.idx2col = idx2col
-            self.num_cols = num_cols
+                                                       desc="Producing melspectrograms for all audios")):
+                melspec = self.get_melspectrogram(audio_path)
+                self.X.append(melspec)
+                regex_res = self.prog.match(audio_path.name)
+                label = regex_res.group(1)[:-1]
+                self.y.extend([label] * melspec.shape[0])
+            self.X = torch.cat(self.X, dim=0)
        
     def __len__(self):
-        return len(self.audio_paths) if not self.iter_over_cols else self.num_cols
+        # return len(self.audio_paths) if not self.iter_over_cols else self.num_cols
+        return len(self.audio_paths) if not self.iter_over_cols else self.X.shape[0]
 
     def __getitem__(self, index):
-        if not self.iter_over_cols:
-            file_idx = index
-        else:
-            file_idx = self.idx2col[index][0]
-        
+        # if not self.iter_over_cols:
+        #     file_idx = index
+        # else:
+        #     file_idx = self.idx2col[index][0]
         res = {}
+        
+        if self.iter_over_cols:
+            return {
+                "input": self.X[index],
+                "label": self.y[index]
+            }
+                              
+        file_idx = index
+
         audio_path = self.audio_paths[file_idx]
         regex_res = self.prog.match(audio_path.name)
         res["label"], res["machine_id"], res["audio_id"] = regex_res.group(1, 2, 3)
@@ -70,24 +105,24 @@ class SliderDataset(Dataset):
         x = self.get_melspectrogram(audio_path)
         L = x.shape[0]
         
-        if not self.iter_over_cols:
-            if L < self.maxlen:
-                x = torch.pad(x, (0, 0, 0, self.maxlen - L))
-            elif L > self.maxlen:
-                x = x[:self.maxlen, :]
+        # if not self.iter_over_cols:
+        if L < self.maxlen:
+            x = torch.pad(x, (0, 0, 0, self.maxlen - L))
+        elif L > self.maxlen:
+            x = x[:self.maxlen, :]
 
-            if self.use_cnn:
-                x = x.unsqueeze(0)
-                res["input"] = x
-            else:
-                res["input"] = x[:-1, :]
-                res["target"] = x[1:, :]
+        if self.use_cnn:
+            x = x.unsqueeze(0)
+            res["input"] = x
         else:
-            col_idx = self.idx2col[index][1]
-            res["input"] = x[col_idx, :]
+            res["input"] = x[:-1, :]
+            res["target"] = x[1:, :]
+        # else:
+        #     col_idx = self.idx2col[index][1]
+        #     res["input"] = x[col_idx, :]
         return res
 
-    @lru_cache(maxsize=500)
+    # @lru_cache(maxsize=10000)
     def get_melspectrogram(self, audio_path):
         wav, sr = torchaudio.load(audio_path)
         # print(f"{sr=}")
@@ -100,8 +135,17 @@ class SliderDataset(Dataset):
                                          normalized=False)
         res = melspec_transformer(wav).squeeze().T
         res = T.AmplitudeToDB(top_db=80., stype="power")(res)
+        # res = 20.0 / self.power * torch.log10(res + sys.float_info.epsilon)
         if self.normalize:
             min_level_db = -100
             res = torch.clip((res - min_level_db) / -min_level_db, 0, 1)
+        
+        # n_frames = 5
+        # frame_len = res.shape[0] - n_frames + 1
+        # frames = []
+        # for i in range(n_frames):
+        #     frames.append(res[i : frame_len + i, :]) 
+        # res = torch.cat(frames, dim=1)
+        # print(res.shape)
 
         return res
